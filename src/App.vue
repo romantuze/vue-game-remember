cf2412afc9193e36db109e9a92b34ef49b614d5a
romantuze <romantuze@gmail.com>
2021-08-05 15:49:12 +0400

    Start project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="wrapper">
+  <h1>Game Remember me</h1>
+  <p>Запомни и открой все клетки</p>
+
+
+
+  <Board />
+</div>
+
+
+
+
+
+</template>
+
+<script>
+import Board from './components/Board.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Board
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
